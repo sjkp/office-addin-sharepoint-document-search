ad7954c988f8805f95142a47a4b032613a2fa562
Simon Pedersen <blasimtex@hotmail.com>
2015-09-15 21:25:39 +0200

    updated

--- app/app.config.js
+++ app/app.config.js
@@ -1,7 +1,7 @@
 (function () {
-	var tenantName = 'contoso';
+	var tenantName = 'delegatedemo01';
 	
 	var officeAddin = angular.module('officeAddin');
-	officeAddin.constant('appId', '00000000-0000-0000-0000-000000000000');
+	officeAddin.constant('appId', '355ae2c7-3ebe-4935-b09b-1ce62d170e35');
 	officeAddin.constant('sharePointUrl', 'https://' + tenantName + '.sharepoint.com');
 })();
